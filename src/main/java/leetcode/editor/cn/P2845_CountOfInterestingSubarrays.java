61cf90aa10b3fefbf93c7a488159a577a0d1687f
baiYanXi <dingdo.yy@qq.com>
2025-04-25 17:56:58 +0800

    :art: 2845

---
+++ src/main/java/leetcode/editor/cn/P2845_CountOfInterestingSubarrays.java
@@ -0,0 +1,25 @@
+package leetcode.editor.cn;
+
+
+import java.util.List;
+
+public class P2845_CountOfInterestingSubarrays {
+    
+    public static void main(String[] args) {
+        Solution solution = new P2845_CountOfInterestingSubarrays().new Solution();
+        // put your test code here
+        
+    }
+    
+        //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public long countInterestingSubarrays(List<Integer> nums, int modulo, int k) {
+
+            return 1L;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+    
+    
+}
